3257f28b541ec4011959708c04e4d5b46686cc8d
EbeltramoUtn <166845082+EbeltramoUtn@users.noreply.github.com>
2025-11-01 13:58:42 -0300

    Integrate RabbitMQ for donation event handling (#8)
    Added RabbitMQ configuration, DTOs, and consumer to process donation status updates via messaging. Updated campaign service to handle donation events and adjust campaign amounts accordingly. Updated dependencies and application properties for RabbitMQ support.

--- src/main/java/com/platform/campaign_service/CampaignServiceApplication.java
+++ src/main/java/com/platform/campaign_service/CampaignServiceApplication.java
@@ -1,5 +1,6 @@
 package com.platform.campaign_service;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableRabbit
 public class CampaignServiceApplication {
     /**
      * The main entry point of the Campaign Service application.
