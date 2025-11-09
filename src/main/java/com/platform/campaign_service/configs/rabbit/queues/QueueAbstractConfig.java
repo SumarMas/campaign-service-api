660381e48000a655c20d05f0f1994f17b0663a4f
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2025-11-09 19:49:43 -0300

    Update QueueAbstractConfig.java

--- src/main/java/com/platform/campaign_service/configs/rabbit/queues/QueueAbstractConfig.java
+++ src/main/java/com/platform/campaign_service/configs/rabbit/queues/QueueAbstractConfig.java
@@ -1,11 +1,7 @@
 package com.platform.campaign_service.configs.rabbit.queues;
 
 import lombok.Getter;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
